872c6eef7d9c1eda5e7669aa66fa14a84c31cc8a
koborochka <098mishawifi@gmail.com>
2025-03-07 14:20:17 +0500

    initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <p className="text-cyan-700">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
+
